3d0dd73c60a21960b4f586749db1336756ada0c0
Adrián Fernández García <adferdev@gmail.com>
2023-10-10 09:48:49 +0200

    Update lsp.lua

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -27,10 +27,6 @@ lsp.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
   lsp.default_keymaps({buffer = bufnr})
-end)lsp.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp.default_keymaps({buffer = bufnr})
 end)
 
 lsp.setup()

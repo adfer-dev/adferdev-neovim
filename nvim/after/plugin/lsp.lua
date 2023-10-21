a7e658ac4c76afa5841c3df8e2e15bf7cb3741bc
Adrián Fernández García <adferdev@gmail.com>
2023-10-21 18:04:09 +0200

    Update lsp.lua to select first option by default

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -11,6 +11,13 @@ require('mason-lspconfig').setup({
 })
 
 local cmp = require("cmp")
+cmp.setup({
+  preselect = 'item',
+  completion = {
+    completeopt = 'menu,menuone,noinsert'
+  },
+})
+
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
